4d26278451420e226dff2ce6d5cc6a07712588ba
Mohammad Alaei <alaeimono@gmail.com>
2023-10-17 18:55:31 +0330

    Last commit 0

--- src/components/serialPort.js
+++ src/components/serialPort.js
@@ -4,19 +4,35 @@ import React, { useState } from "react";
 import CarImage from "./carImage";
 
 const SerialPortReader = () => {
-  const [sensorValues, setSensorValues] = useState({  time:0, 
-                                                      AccX: 0, 
-                                                      AccY: 0, 
-                                                      AccZ: 0, 
-                                                      GyroX:0, 
-                                                      GyroY:0, 
-                                                      GyroZ:0 }); 
+  // const [sensorValues, setSensorValues] = useState({  time:0, 
+  //                                                     AccX: 0, 
+  //                                                     AccY: 0, 
+  //                                                     AccZ: 0, 
+  //                                                     GyroX:0, 
+  //                                                     GyroY:0, 
+  //                                                     GyroZ:0 }); 
   const [sensorIsConnected, setSensorIsConnected] = useState(false);
   const [data, setData] = useState("");
   const [baudRate, setBaudRate] = useState(115200);
+  const [command, setCommand] = useState('');
+  const [serialWriter, setSerialWriter] = useState(null);
+  const [serialReader, setSerialReader] = useState(null);
+
 
   const handleSetBaudRate = (newBaudRate)=>{
     setBaudRate(newBaudRate);
+  }
+
+  const handleSetCommand = async (newCommand)=>{
+    setCommand(newCommand);
+
+    if (serialWriter){
+      const encoder = new TextEncoder();
+      await serialWriter.write(encoder.encode(newCommand));
+      if (command === 'M'){
+        readLines();
+      }
+    }
   }
 
   const handleSelectComPort = async () => {
@@ -24,32 +40,42 @@ const SerialPortReader = () => {
       const port = await navigator.serial.requestPort();
       await port.open({ baudRate: baudRate });
       const reader = port.readable.getReader();
+      const writer = port.writable.getWriter();
+      setSerialWriter(writer);
+      setSerialReader(reader);
       setSensorIsConnected(true);
-      readLines(reader);
-
     } catch(error){
       console.error('Serial port permission denied:', error);
       setSensorIsConnected(false);
     }
   };
-  async function readLines(reader) {
+
+  async function readLines() {
     const textDecoder = new TextDecoder('utf-8');
     let partialLine = ''; 
+    console.error('Start Reading...');
 
     while (true) {
         try{
-          const { value, done } = await reader.read();
-          if (done) break;
-          const text = textDecoder.decode(value);
-          const lines = (partialLine + text).split('\n');
-          for (let i = 0; i < lines.length - 1; i++) {
-              const line = lines[i].trim();
-              if (line) {
-                  processData(line);
+          if(command === 'M'){
+              setSensorIsConnected(true);
+              const { value, done } = await serialReader.read();
+              if (done) break;
+              const text = textDecoder.decode(value);
+              const lines = (partialLine + text).split('\n');
+              for (let i = 0; i < lines.length - 1; i++) {
+                  const line = lines[i].trim();
+                  if (line) {
+                      processData(line);
+                  }
               }
+              partialLine = lines[lines.length - 1];
           }
-          partialLine = lines[lines.length - 1];
-          setSensorIsConnected(true);
+          else{
+            console.error('Stop Reading');
+            setSensorIsConnected(false);
+            break;
+          }
         }catch(error){
           console.error('Serial port disconnected:', error);
           setSensorIsConnected(false);
@@ -64,6 +90,7 @@ function getCurrentTime() {
 }
 
   function processData(line) {
+    console.log(line);
     const dataParts = line.split('|');
     if (dataParts.length === 6) {
         const accX = parseFloat(dataParts[0].split(':')[1]);
@@ -77,7 +104,7 @@ function getCurrentTime() {
         
         setData((prevState) => prevState + "\n" + line);
 
-        setSensorValues({ time:time, AccX: accX, AccY: accY, AccZ: accZ, GyroX: gyroX, GyroY: gyroY, GyroZ: gyroZ });
+        // setSensorValues({ time:time, AccX: accX, AccY: accY, AccZ: accZ, GyroX: gyroX, GyroY: gyroY, GyroZ: gyroZ });
     }
 }
 
@@ -89,9 +116,14 @@ function getCurrentTime() {
         Select COM Port
       </button>
   </div>
-  <div className="input-group d-flex align-items-center justify-content-center mt-2" >
-        <input type="text" className="form-control" width="25%" aria-label="Baud Rate" placeholder="BaudRate" value={baudRate} onChange={(e)=>handleSetBaudRate(e.target.value)}/>
+  <div className="input-group row d-flex align-items-center justify-content-center mt-2" >
+        <input type="text" className="col-6 col-md-6 col-lg-6 form-control mx-2" width="25%" aria-label="Baud Rate" placeholder="BaudRate" value={baudRate} onChange={(e)=>handleSetBaudRate(e.target.value)}/>
+        {/* <input type="text" className="col-6 col-md-6 col-lg-6 form-control" width="25%" aria-label="Command" placeholder="Command" value={command} onChange={(e)=>handleSetCommand(e.target.value)}/> */}
   </div>
+  <button disabled={!sensorIsConnected} className="btn btn-success rounded mt-2" onClick={(e)=>handleSetCommand('M')} >Start</button>
+  <button disabled={!sensorIsConnected} className="btn btn-warning rounded mt-2 mx-2" onClick={(e)=>handleSetCommand('S')}>Stop</button>
+
+
   <div key="Acc-Row" className="row">
     <div className="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6">
       <div hidden={sensorIsConnected ? false : true}>
